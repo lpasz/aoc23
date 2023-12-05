c2bc294a1ae035d5454236f6200c6ae9e129ba85
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2023-12-05 17:43:47 -0300

    this mess works, somehow

--- src/aoc23/day5.clj
+++ src/aoc23/day5.clj
@@ -6,20 +6,26 @@
 (def exp1-input (slurp "./inputs/day5/exp1.txt"))
 (def part1-input (slurp "./inputs/day5/part1.txt"))
 
+(defn in-between-range [[start range-size]]
+  #(or (= start %) (< start % (+ start range-size))))
+
 (defn in-between [[start1 start2 range-size]]
-  [#(or (= start2 %) (< start2 % (+ start2 range-size)))
-   #(+ start1 (- % start2))])
+  [(in-between-range [start2 range-size])
+   #(+ start1 (- % start2))
+   (in-between-range [start1 range-size])
+   #(+ start2 (- % start1))])
 
 (defn to-funcs [lines]
   (->> (rest lines)
        (map #(re-seq #"\d+" %))
-       (map (fn [ranges] (map bigdec ranges)))
+       (map (fn [ranges] (map biginteger ranges)))
        (map in-between)))
 
-
-;; (let [[is-in-range get-value] (in-between [52 50 48])]
-;;   [(is-in-range 79)
-;;    (get-value 79)])
+(defn to-ranges [lines]
+  (->> (rest lines)
+       (map #(re-seq #"\d+" %))
+       (map (fn [ranges] (map biginteger ranges)))
+       (map (fn [start]))))
 
 (defn parse-input [inp]
   (let [[[seeds]
@@ -31,7 +37,7 @@
          temperature-to-humidity
          humidity-to-location] (->> (str/split inp #"\n\n")
                                     (map #(str/split-lines %)))]
-    {:seeds (->> seeds (re-seq #"\d+") (map bigdec))
+    {:seeds (->> seeds (re-seq #"\d+") (map biginteger))
      :seed-to-soil (to-funcs seed-to-soil)
      :soil-to-fertilizer (to-funcs soil-to-fertilizer)
      :fertilizer-to-water (to-funcs fertilizer-to-water)
@@ -40,10 +46,18 @@
      :temperature-to-humidity (to-funcs temperature-to-humidity)
      :humidity-to-location (to-funcs humidity-to-location)}))
 
+(parse-input part1-input)
+
 (defn gets [coll-fn k dv]
   (or (->> coll-fn
            (filter (fn [[in-range _remaped-value]] (in-range k)))
            (keep (fn [[_in-range remaped-value]] (remaped-value k)))
+           (first)) dv))
+
+(defn gets-reversed [coll-fn k dv]
+  (or (->> coll-fn
+           (filter (fn [[_ _ in-range _]] (in-range k)))
+           (keep (fn [[_ _ _ remaped-value]] (remaped-value k)))
            (first)) dv))
 
 (defn get-seed-location [seed {:keys [seed-to-soil
@@ -62,39 +76,91 @@
         location (gets humidity-to-location humidity humidity)]
     location))
 
-(defn almanaque-seeds [almanaque]
-  (for [seed (:seeds almanaque)]
-    (get-seed-location seed almanaque)))
+(defn get-location-seed [location {:keys [seed-to-soil
+                                          soil-to-fertilizer
+                                          fertilizer-to-water
+                                          water-to-light
+                                          light-to-temperature
+                                          temperature-to-humidity
+                                          humidity-to-location]}]
+  (let [humidity (gets-reversed humidity-to-location location location)
+        temperature (gets-reversed temperature-to-humidity humidity humidity)
+        light (gets-reversed light-to-temperature temperature temperature)
+        water (gets-reversed water-to-light light light)
+        fertilizer (gets-reversed fertilizer-to-water water water)
+        soil (gets-reversed soil-to-fertilizer fertilizer fertilizer)
+        seed (gets-reversed seed-to-soil soil soil)]
+    seed))
 
-(defn part1 [inp]
-  (->> (parse-input inp)
-       (almanaque-seeds)
-       (apply min)))
-
-(defn remap-seeds [almanaque]
-  (assoc almanaque :seeds (lazy-cat
-                           (->> (:seeds almanaque)
-                                (partition 2)
-                                (mapcat (fn [[s rng]]
-                                          (range s (+ s rng))))))))
-
-(defn closest-spot [almanaque]
+(defn closest-spot [seeds almanaque]
   (reduce (fn [closest-location seed]
             (let [location (get-seed-location seed almanaque)]
               (if (nil? closest-location)
                 location
                 (min closest-location location))))
           nil
-          (:seeds almanaque)))
+          seeds))
 
-(defn part2 [inp]
-  (->> (parse-input inp)
-       (remap-seeds)
-       (closest-spot)))
+(defn part1 [inp]
+  (let [almanaque (parse-input inp)]
+    (closest-spot (:seeds almanaque) almanaque)))
+
+(defn remap-seeds [almanaque]
+  (-> almanaque
+      (assoc :seeds-in-between-fns (->> (:seeds almanaque)
+                                        (partition 2)
+                                        (map in-between-range)))
+      (assoc :seeds
+             (->> (:seeds almanaque)
+                  (partition 2)
+                  (mapcat (fn [[s rng]] [s (+ s rng)]))))))
+
+(defn part2 [inp range]
+  (->> (parse-input inp)))
 
 (part1 exp1-input)
 (part1 part1-input)
 
+(def almanaque (remap-seeds (parse-input part1-input)))
+(def almanaque-exp (remap-seeds (parse-input exp1-input)))
 
-(part2 exp1-input)
-(part2 part1-input)
+
+(defn is-seed? [seed]
+  (some #(% seed) (:seeds-in-between-fns almanaque-exp)))
+
+(get-location-seed 32 almanaque-exp)
+
+(def step 1000)
+
+(def seeds 
+  (->> )
+  (lazy-cat
+            (range 2637529854 (+ 2637529854 223394899) step)
+            (range 3007537707 (+ 3007537707 503983167) step)
+            (range 307349251 (+ 307349251 197383535) step)
+            (range 3543757609 (+ 3543757609 276648400) step)
+            (range 2296792159 (+ 2296792159 141010855) step)
+            (range 116452725 (+ 116452725 5160533) step)
+            (range 2246652813 (+ 2246652813 49767336) step)
+            (range 762696372 (+ 762696372 160455077) step)
+            (range 3960442213 (+ 3960442213 105867001) step)
+            (range 1197133308 (+ 1197133308 38546766) step)))
+
+;; jumping each 1000 we got that the smallest location found is 79874951
+(assert (= 79875353 (closest-spot seeds almanaque)))
+;; we get the seed for that location
+(assert (= 3969172213N (get-location-seed 79875353 almanaque)))
+;; with the location in hand we check around it, since we did not found any lower than 
+(->> (for [seed (range (- 3969172213 20000) 3969172213)]
+       [seed (get-seed-location seed almanaque)])
+     (sort-by second))
+
+
+(get-seed-location 3969171811 almanaque)
+
+
+
+
+116452725 ;; lowest seed
+3960442213 ;; highest seed
+
