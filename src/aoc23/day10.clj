b320b07267e465e36b7fa58d8339ecdcb4f513ca
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2023-12-10 19:44:41 -0300

    day 10 improved

--- src/aoc23/day10.clj
+++ src/aoc23/day10.clj
@@ -1,6 +1,5 @@
 (ns aoc23.day10
-  (:require [core :as c]
-            [clojure.set :as set]))
+  (:require [core :as c]))
 
 (def exp1-input (c/get-input "exp1.txt"))
 (def exp2-input (c/get-input "exp2.txt"))
@@ -9,93 +8,53 @@
 (def exp4-input (c/get-input "exp4.txt"))
 (def exp5-input (c/get-input "exp5.txt"))
 
-(defn find-start
-  ([mtx] (find-start mtx \S))
-  ([mtx value]
-   (->> mtx
-        (filter #(= value (val %)))
-        (ffirst))))
+(defn- find-start [mtx]
+  (->> mtx
+       (filter #(= \S (val %)))
+       (ffirst)))
 
-(defn next-directions [[x y]]
-  {:north [x (dec y)]
-   :west [(dec x) y]
-   :east [(inc x) y]
-   :south [x (inc y)]})
+(defn- next-directions [[x y]]
+  {:north [x (dec y)] :west [(dec x) y] :east [(inc x) y] :south [x (inc y)]})
 
-(def moves {\| {:south :south :north :north};; is a vertical pipe connecting north and south.
-            \- {:east :east :west :west};; is a horizontal pipe connecting east and west.
-            \L {:south :east :west :north};; is a 90-degree bend connecting north and east.
-            \J {:south :west :east :north};; is a 90-degree bend connecting north and west.
-            \7 {:north :west :east :south};; is a 90-degree bend connecting south and west.
-            \F {:north :east :west :south};; is a 90-degree bend connecting south and east.
-            })
+(def pipe-directions {\| {:south :south :north :north} ;; is a vertical pipe connecting north and south.
+                      \- {:east :east :west :west} ;; is a horizontal pipe connecting east and west.
+                      \L {:south :east :west :north} ;; is a 90-degree bend connecting north and east.
+                      \J {:south :west :east :north} ;; is a 90-degree bend connecting north and west.
+                      \7 {:north :west :east :south} ;; is a 90-degree bend connecting south and west.
+                      \F {:north :east :west :south} ;; is a 90-degree bend connecting south and east.
+                      })
 
+(defn- walk-one-step [mtx [direction position]]
+  (let [current-pipe (get mtx position)
+        next-direction (get-in pipe-directions [current-pipe direction])
+        next-position (get (next-directions position) next-direction)]
+    (when (and current-pipe
+               next-direction
+               next-position)
+      [next-direction next-position])))
 
-
-(defn two-in-same-pos [curr-pos]
-  (first (reduce (fn [[repeated acc] [_direction value]]
-                   (cond
-                     (not (nil? repeated)) [repeated acc]
-                     (acc value) [value acc]
-                     :else [repeated (conj acc value)]))
-                 [nil #{}]
-                 curr-pos)))
-
-(defn walk-pipe [start mtx]
-  (loop [curr-pos (next-directions start)
-         distance 1]
-    (let [next-pos (->> curr-pos
-                        (keep (fn [[direction position]]
-                                (when-let [current-pipe (get mtx position)]
-                                  (when-let [next-direction (get-in moves [current-pipe direction])]
-                                    [next-direction (get (next-directions position) next-direction)])))))]
-      (if (two-in-same-pos next-pos)
-        (inc distance)
-        (recur next-pos (inc distance))))))
-
-
-(defn find-loop [mtx positions]
-  (loop [current-possible-possitions (next-directions (first positions))
-         positions positions]
-    (let [[next-dir next-pos curr-pos] (->> current-possible-possitions
-                                            (keep (fn [[direction position]]
-                                                    (when-let [current-pipe (get mtx position)]
-                                                      (when-let [next-direction (get-in moves [current-pipe direction])]
-                                                        [next-direction
-                                                         (get (next-directions position) next-direction)
-                                                         position]))))
-                                            (first))]
-      (if ((set positions) next-pos)
-        (conj positions curr-pos)
-        (recur [[next-dir next-pos]]
-               (conj positions next-pos))))))
-
-
-(defn walk-one-step  [mtx [direction position]]
-  (when-let [current-pipe (get mtx position)]
-    (when-let [next-direction (get-in moves [current-pipe direction])]
-      [next-direction
-       (get (next-directions position) next-direction)])))
-
-(defn next-valid-position [mtx position]
+(defn- find-next-valid-step
+  "try to find a valid next step, and it's direction"
+  [mtx position]
   (->> (next-directions position)
        (filter #(walk-one-step mtx %))
        (first)))
 
-(defn find-loop-2 [mtx start]
-  (let [next #(walk-one-step mtx %)
-        [dir pos] (next-valid-position mtx start)]
-    (loop [curr [dir pos]
-           positions [start]]
-      (let [[_direction position] curr]
-        (if ((set positions) position)
-          positions
-          (recur (next curr)
-                 (conj positions position)))))))
+(defn- find-pipe-loop
+  "get all position that form the pipe loop"
+  [mtx start-position]
+  (loop [curr-dir-pos (find-next-valid-step mtx start-position)
+         positions [start-position]]
+    (let [[_direction position] curr-dir-pos]
+      (if (= start-position position)
+        positions
+        (recur (walk-one-step mtx curr-dir-pos)
+               (conj positions position))))))
 
 
-(defn shoelaces-area
-  "See more: https://en.wikipedia.org/wiki/Shoelace_formula"
+(defn- shoelaces-theorem-area
+  "We are using the polygon version.
+   See more: https://en.wikipedia.org/wiki/Shoelace_formula"
   [polygon-points]
   (->> polygon-points
        (partition 2 1)
@@ -104,22 +63,23 @@
        (c/then [n] (quot n 2))
        (abs)))
 
-(defn pick-internal-points
-  "See more: https://en.wikipedia.org/wiki/Pick%27s_theorem"
+(defn- pick-theorem-internal-points
+  "Normaly used to find the area, since we found the area with shoelace, we are using it to get the number of internal points.
+   See more: https://en.wikipedia.org/wiki/Pick%27s_theorem"
   [area polygon-points]
   (- area (- (quot (count polygon-points) 2) 1)))
 
-
 (defn part1 [inp]
-  (->> (c/to-matrix inp)
-       (c/then [mtx] (walk-pipe (find-start mtx) mtx))))
+  (let [mtx (c/to-matrix inp)
+        pipe-loop-points (find-pipe-loop mtx (find-start mtx))]
+    (quot (count pipe-loop-points) 2)))
 
 (defn part2 [inp]
   (let [mtx (c/to-matrix inp)
-        loop-points (find-loop-2 mtx (find-start mtx))
-        area (shoelaces-area loop-points)]
-    (pick-internal-points area loop-points)))
-
+        pipe-loop-points (find-pipe-loop mtx (find-start mtx))
+        pipe-loop-area (shoelaces-theorem-area pipe-loop-points)
+        points-inside-loop (pick-theorem-internal-points pipe-loop-area pipe-loop-points)]
+    points-inside-loop))
 
 (comment
   (assert (= 4 (part1 exp1-input)))
@@ -132,5 +92,3 @@
   (assert (= 417 (part2 part1-input)))
   ;;
   )
-
-
