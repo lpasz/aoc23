80e7ff6871b130d71f056c214f07437f769aaac7
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2023-12-13 11:58:11 -0300

    day 13 part 2

--- src/aoc23/day13.clj
+++ src/aoc23/day13.clj
@@ -20,7 +20,7 @@
     (->> (partition 2 1 idx-mtx)
          (filter (fn [[[idx1 line1] [idx2 line2]]] (reflect? idx-mtx idx1 line1 idx2 line2)))
          (first)
-         (c/then [[[idx1 _line1] [idx2 _line2]]] [idx1 idx2]))))
+         (c/then [[[idx1 _line1] [_idx2 _line2]]] idx1))))
 
 (defn reflections-columns [mtx reflection?]
   (reflections-lines (c/transpose mtx) reflection?))
@@ -32,13 +32,9 @@
 (defn to-mtx-with-coords [lines]
   (map (fn [line] (seq line)) (str/split-lines lines)))
 
-(defn calc-reflections [reflections]
-  (cond
-    (= [nil nil] (:lines reflections))
-    (first (:columns reflections))
-
-    (= [nil nil] (:columns reflections))
-    (* 100 (first (:lines reflections)))))
+(defn calc-reflections [{:keys [lines columns]}]
+  (cond (nil? lines) columns
+        (nil? columns) (* 100 lines)))
 
 (defn diff [line1 line2]
   (->> (map #(= %1 %2) line1 line2)
@@ -50,7 +46,8 @@
   ([idx-mtx idx1 line1 idx2 line2 acc-diff]
 ;;    (c/insp [line1 line2 acc-diff (diff line1 line2) (= line1 line2)])
    (cond
-     (or (nil? line1) (nil? line2)) true
+     (and (= acc-diff 1) (or (nil? line1) (nil? line2))) true
+     (or (nil? line1) (nil? line2)) false
      (= line1 line2) (recur idx-mtx
                             (dec idx1) (get idx-mtx (dec idx1))
                             (inc idx2) (get idx-mtx (inc idx2))
@@ -71,13 +68,17 @@
 (defn part2 [inp]
   (->> (str/split inp #"\n\n")
        (map to-mtx-with-coords)
-       (map #(find-reflections % reflection-diff?))))
+       (map #(find-reflections % reflection-diff?))
+       (map calc-reflections)
+       (reduce +)))
 
 (assert (= 405 (part1 exp1-input)))
 (assert (= 29846 (part1 part1-input)))
 
+(part2 part1-input)
 
-(defn calc-reflections1 [reflections]
+
+(defn calc-reflections1 [{:keys [lines columns]}]
   (cond
     (= [nil nil] (:lines reflections))
     (or (first (:columns reflections)) 0)
@@ -85,21 +86,22 @@
     (= [nil nil] (:columns reflections))
     (* 100 (or (first (:lines reflections)) 0))))
 
-(->> (map (fn [r1 r2]
-            (cond
-              (= (:lines r1) (:lines r2))
-              {:lines [nil nil], :columns (:columns r2)}
+(->> (map (fn [r1 r2] [r1 r2])
 
-              (= (:columns r1) (:columns r2))
-              {:lines (:lines r2), :columns [nil nil]}
+      ;; (fn [r1 r2]
+      ;;       [r1 ]
+      ;;       (cond
+      ;;         (= (:lines r1) (:lines r2))
+      ;;         {:lines [nil nil], :columns (:columns r2)}
 
-              :else nil))
+      ;;         (= (:columns r1) (:columns r2))
+      ;;         {:lines (:lines r2), :columns [nil nil]}
+
+      ;;         :else r2))
           (->> (str/split part1-input #"\n\n")
                (map to-mtx-with-coords)
                (map #(find-reflections % reflection?)))
-          (part2 part1-input))
-     (map calc-reflections1)
-     (reduce +))
+          (mp)(part2 part1-input)))
 
 (filter #(=  {:lines [nil nil], :columns [nil nil]} %) (part2 part1-input))
 
