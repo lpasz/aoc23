e08573af2aa2c69f219f6107bb979094cfcba874
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2023-12-15 14:44:30 -0300

    improve day 15

--- src/aoc23/day15.clj
+++ src/aoc23/day15.clj
@@ -1,4 +1,5 @@
 (ns aoc23.day15
+  "Lens Library"
   (:require [core :as c]
             [clojure.string :as str]))
 
@@ -8,20 +9,12 @@
 (defn hash-calc [char hash-acc]
   (rem (* 17 (+ hash-acc (long char))) 256))
 
-(defn hash-str [s]
-  (loop [s s h 0]
-    (let [s1 (first s)
-          srest (rest s)]
-      (if (nil? s1)
-        h
-        (if (= s1 \newline)
-          (recur srest h)
-          (recur srest (hash-calc s1 h)))))))
-
-(defn part1 [inp]
-  (->> (str/split inp #"\n|,")
-       (map hash-str)
-       (reduce +)))
+(defn hash-str
+  ([s] (hash-str s 0))
+  ([[s1 & srest] hash]
+   (if (nil? s1)
+     hash
+     (recur srest (hash-calc s1 hash)))))
 
 (defn add-key-hash-focal-or-update-focal [coll key hash-key focal]
   (if (some #(= key (first %)) coll)
@@ -33,31 +26,47 @@
 (defn remove-lens-with-key [coll key]
   (c/remove-first #(#{key} (first %)) coll))
 
-(defn hashmap [strs]
-  (loop [strs strs
-         acc (sorted-map)]
-    (if (nil? (first strs))
-      acc
-      (let [[[_all key operation focal]] (->>  strs (first) (re-seq #"([A-Z|a-z]+)([=|-])(\d+)?"))
-            hash-key (hash-str key)
-            focal (c/parse-int focal)]
+(defn parse-lens-box [lens-box]
+  (let [[[_all key operation focal]] (re-seq #"([A-Z|a-z]+)([=|-])(\d+)?" lens-box)]
+    [key
+     operation
+     (hash-str key)
+     (c/parse-int focal)]))
+
+(defn hashmap [lens-boxes]
+  (loop [lens-boxes lens-boxes
+         acc {}]
+    (if-let [lens-box (first lens-boxes)]
+      (let [[key operation hash-key focal] (parse-lens-box lens-box)]
         (case operation
-          "=" (recur (rest strs)
+          "=" (recur (rest lens-boxes)
                      (update acc (hash-str key) #(add-key-hash-focal-or-update-focal % key hash-key focal)))
-          "-" (recur (rest strs)
-                     (update acc (hash-str key) #(remove-lens-with-key % key))))))))
+          "-" (recur (rest lens-boxes)
+                     (update acc (hash-str key) #(remove-lens-with-key % key)))))
+      acc)))
+
+(defn part1 [inp]
+  (->> (str/split inp #"\n|,")
+       (map hash-str)
+       (reduce +)))
 
 (defn part2 [inp]
   (->> (str/split inp #"\n|,")
        (hashmap)
+       ;; only the vals matter, we have the box value inside vals too
        (vals)
+       ;; reverse because list insert at the start not at the end
        (map reverse)
+       ;; calculate the focal power
        (mapcat #(map-indexed (fn [slot [_key box focal]]  (* (inc box) (inc slot) focal))  %))
        (reduce +)))
 
-(assert (= 1320 (part1 exp1-input)))
-(assert (= 517965 (part1 part1-input)))
-(assert (= 145 (part2 exp1-input)))
-(assert (= 267372 (part2 part1-input)))
+(comment
+  (assert (= 1320 (part1 exp1-input)))
+  (assert (= 517965 (part1 part1-input)))
+  (assert (= 145 (part2 exp1-input)))
+  (assert (= 267372 (part2 part1-input)))
+;;
+  )
 
 
