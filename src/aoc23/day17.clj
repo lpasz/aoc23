937a5c850059d032f21c1e5271ae9383ac20c18d
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2023-12-17 20:49:14 -0300

    day 17 improvements

--- src/aoc23/day17.clj
+++ src/aoc23/day17.clj
@@ -1,70 +1,67 @@
 (ns aoc23.day17
   "Clumsy Crucible"
   (:require [core :as c]
-            [complex.core :as cc]
-            [clojure.string :as str]
             [clojure.data.priority-map :refer [priority-map]]))
 
 (def exp1-input (c/get-input "exp1.txt"))
 (def part1-input (c/get-input "part1.txt"))
 
-(def j (cc/complex 0 1))
-(def -j (cc/complex 0 -1))
+(def directions {:up #{:-> :<-}
+                 :down #{:-> :<-}
+                 :-> #{:up :down}
+                 :<- #{:up :down}})
 
-(defn to-complex-matrix [inp]
-  (->> (for [[idx row] (map-indexed (fn [idx row] [idx row]) (str/split-lines inp))]
-         (for [[idy column] (map-indexed (fn [idy column] [idy (c/parse-int column)]) (re-seq #"\d" row))]
-           [(cc/+ idx (cc/* idy (cc/complex 0 1))) column]))
-       (c/flatten-once)
-       (into {})))
+(defn- next-coord [x y n dir]
+  (case dir
+    :up   [x (- y n)]
+    :down [x (+ y n)]
+    :->   [(+ x n) y]
+    :<-   [(- x n) y]))
 
-(defn recalc [x y dx dy heat-loss deep min max mtx]
-  (let [coord (cc/complex x y)
-        dir (cc/complex dx dy)]
-    (->> (for [next-dir [(cc// j dir) (cc// -j dir)]]
-           (for [i (range min (inc max))
-                 :let [next-coord (cc/+ coord (cc/* next-dir i))]
-                 :when (contains? mtx next-coord)]
-             [(cc/real-part next-coord)
-              (cc/imaginary-part next-coord)
-              (cc/real-part next-dir)
-              (cc/imaginary-part next-dir)
-              (+ heat-loss (->> (range 1 (inc i))
-                                (map #(get mtx (cc/+ coord (cc/* next-dir %))))
-                                (reduce +)))
-              (inc deep)]))
-         (flatten)
-         (partition 6))))
+(defn- recalc [heat-loss deep x y dir min max mtx]
+  (->> (for [ndir (dir directions)]
+         (for [i (range min (inc max))
+               :let [ncoord  (next-coord x y i ndir)]
+               :when (and (not= ncoord [x y]) (contains? mtx ncoord))]
+           [(+ heat-loss (->> (range 1 (inc i))
+                              (map #(next-coord x y % ndir))
+                              (map #(get mtx %))
+                              (reduce +)))
+            (inc deep)
+            ncoord
+            ndir]))
+       (flatten)
+       (partition 5)))
 
-(defn dijkstra [min max end mtx]
-  (loop [queue (priority-map [0 0 0.0 0.0 1.0 0.0]
-                             [0 0 0.0 0.0 1.0 0.0]
-                             [0 0 0.0 0.0 1.0 0.0]
-                             [0 0 0.0 0.0 1.0 0.0])
+(defn- dijkstra [min max end mtx]
+  (loop [queue (priority-map [0 0 0 0 :->]   [0 0 0 0 :->]
+                             [0 0 0 0 :down] [0 0 0 0 :down])
          seen #{}]
-    (when-let [[[heat-loss deep x y dx dy] _] (first queue)]
+    (when-let [[[heat-loss deep x y dir] _] (first queue)]
       (cond (= [x y] end) heat-loss
-            (seen [x y dx dy]) (recur (pop queue) seen)
-            :else (recur (reduce (fn [acc [x y dx dy heat-loss deep]]
-                                   (assoc acc [heat-loss deep x y dx dy] [heat-loss deep x y dx dy]))
-                                 (dissoc queue [heat-loss deep x y dx dy])
-                                 (recalc x y dx dy heat-loss deep min max mtx))
-                         (conj seen [x y dx dy]))))))
+            (seen [x y dir]) (recur (pop queue) seen)
+            ;; (< 10 (count queue)) :stop
+            :else (recur (reduce (fn [acc [heat-loss deep x y dir]]
+                                   (assoc acc
+                                          [heat-loss deep x y dir]
+                                          [heat-loss deep x y dir]))
+                                 (dissoc queue [heat-loss deep x y dir])
+                                 (recalc heat-loss deep x y dir min max mtx))
+                         (conj seen [x y dir]))))))
 
 (defn part1 [inp end-at]
-  (->> inp
-       (to-complex-matrix)
+  (->> (c/to-matrix inp #(re-seq #"\d" %) c/parse-int)
        (dijkstra 1 3 end-at)))
 
 (defn part2 [inp end-at]
-  (->> inp
-       (to-complex-matrix)
+  (->> (c/to-matrix inp #(re-seq #"\d" %) c/parse-int)
        (dijkstra 4 10 end-at)))
 
 (comment
-  (assert (= 102 (part1 exp1-input [12.0 12.0])))
-  (assert (= 742 (part1 part1-input [140.0 140.0])))
-  (assert (= 94 (part2 exp1-input [12.0 12.0])))
-  (assert (= 918 (part2 part1-input [140.0 140.0])))
+  (assert (= 102 (part1 exp1-input [12 12])))
+  (assert (= 742 (part1 part1-input [140 140])))
+  (assert (= 94 (part2 exp1-input [12 12])))
+  (assert (= 918 (part2 part1-input [140 140])))
   ;;
   )
+
