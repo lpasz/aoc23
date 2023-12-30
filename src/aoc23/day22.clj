f2187e1c2e98879a66545ba041713c1847928e36
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2023-12-30 16:37:42 -0300

    Day 22 save

--- src/aoc23/day22.clj
+++ src/aoc23/day22.clj
@@ -1,26 +1,33 @@
 (ns aoc23.day22
   "Sand Slabs"
   (:require [core :as c]
-            [clojure.set :as set]))
+            [clojure.set :as set]
+            [clojure.string :as str]))
 
 (def exp1-input (c/get-input "exp1.txt"))
 (def part1-input (c/get-input "part1.txt"))
 (def part1-other-input (c/get-input "part1-other.txt"))
 
-(defn by-z [[_x _y z]] z)
+(defn rangex [p1 p2]
+  (range (min p1 p2) (inc (max p1 p2))))
+
+(defn by-first-z [_id [[_x _y z]]] z)
 
 (defn parse [inp]
   (->> (re-seq #"(?m)(\d+),(\d+),(\d+)\~(\d+),(\d+),(\d+)" inp)
        (map rest)
        (map #(map c/parse-int %))
        (map-indexed (fn [idx [x1 y1 z1 x2 y2 z2]]
-                      (cond (= [x1 y1 z1] [x2 y2 z2]) [idx [[x2 y2 z2]]]
-                            (= [x1 y1]    [x2 y2])    [idx (sort-by by-z (map #(vector x1 y1 %) (range (min z1 z2) (inc (max z1 z2)))))]
-                            (= [x1 z1]    [x2 z2])    [idx (sort-by by-z (map #(vector x1 % z1) (range (min y1 y2) (inc (max y1 y2)))))]
-                            (= [y1 z1]    [y2 z2])    [idx (sort-by by-z (map #(vector % y1 z1) (range (min x1 x2) (inc (max x1 x2)))))])))
+                      [;; id's
+                    ;;    (char (+ 65 idx))
+                       idx
+                       (for [x (rangex x1 x2)
+                             y (rangex y1 y2)
+                             z (rangex z1 z2)]
+                         [x y z])]))
        (into (sorted-map))))
 
-(defn by-first-z [[_ [[_ _ z]]]] z)
+(def p1 (parse part1-input))
 
 (def floor #{1})
 
@@ -30,52 +37,70 @@
                  (dec z))]
     [x y new-z]))
 
-(defn move-down-if-possible [b bset]
-  (let [down (map (fn [[x y z]] [x y (if (floor z) 1 (dec z))]) b)]
-    (when (and (not= down b) (every? #(not (bset %)) down))
-      (or (move-down-if-possible down bset) down))))
+(defn brick-fall [brick occupied-space]
+  (let [brick-after-gravity (map down-one brick)]
+    (cond (= brick brick-after-gravity) nil
+          (some occupied-space brick-after-gravity) nil
+          :else (or (brick-fall brick-after-gravity occupied-space) brick-after-gravity))))
 
-(defn gravity [brick-coords occupied-spaces]
-  (let [brick-coords-down (map down-one brick-coords)]
-    (when-not (or (= brick-coords brick-coords-down)
-                  (some occupied-spaces brick-coords-down))
-      (or (move-down-if-possible brick-coords-down occupied-spaces)
-          brick-coords-down))))
+(defn closest-to-floor [[_ coords]]
+  (->> coords
+       (map last)
+       (reduce + Integer/MAX_VALUE)))
 
-(defn brick-drop [bricks]
-  (loop [bseq (sort-by by-first-z bricks)
-         bricks bricks
-         occupied-space (set (c/flatten-once (vals bricks)))]
-    (if (empty? bseq)
-      bricks
-      (let [[id brick-coords] (first bseq)
-            bseq (rest bseq)
-            bricksn (dissoc bricks id)
-            occupied-space-after (reduce disj occupied-space brick-coords)
-            brick-coords-down (gravity brick-coords occupied-space-after)]
-        (if-not (nil? brick-coords-down)
-          (recur bseq
-                 (assoc bricksn id brick-coords-down)
-                 (reduce conj occupied-space-after brick-coords-down))
-          (recur bseq
-                 bricks
-                 occupied-space))))))
+(defn after-falling [bricks]
+  (loop [bricks (c/queue (sort-by closest-to-floor bricks))
+         occupied-space (->> bricks (vals) (c/flatten-once) (set))
+         after-falling (sorted-map)]
+    (if (empty? bricks)
+      after-falling
+      (let [[id brick] (peek bricks)
+            bricks (pop bricks)
+            occupied-space-rm (reduce disj occupied-space brick)
+            brick-after-fall (brick-fall brick occupied-space-rm)]
+        (if (nil? brick-after-fall)
+          (recur bricks
+                 occupied-space
+                 (assoc after-falling id brick))
+          (recur bricks
+                 (reduce conj occupied-space-rm brick-after-fall)
+                 (assoc after-falling id brick-after-fall)))))))
 
-(def bricks (brick-drop (parse part1-input)))
-(def bricks-other (brick-drop (parse part1-other-input)))
-(def ex-bricks (brick-drop (parse exp1-input)))
+(defn after-falling-all [bricks]
+  (c/insp ".")
+  (let [after (after-falling bricks)]
+    (if (= bricks after)
+      after
+      (recur after))))
+
+(after-falling-all (parse part1-input))
+
+(def bricks (after-falling-all (parse part1-input)))
+(def bricks-other (after-falling (parse part1-other-input)))
+(def ex-bricks (after-falling (parse exp1-input)))
+
+
 
 (defn part1 [bricks]
   (->> (keys bricks)
-       (map (fn [b-id] (print (str "brick id " b-id " "))
-              b-id))
        (map #(dissoc bricks %))
        ;; nothing moved if brick is removed
-       (filter (fn [brickless]
-                 (c/insp (= brickless (brick-drop brickless)))))
-       (count)))
+       (filter #(not= % (after-falling %)))
+       (count)
+       (- (count bricks))))
 
-(part1 ex-bricks)
+(part1 bricks)
+
+
+(defn parse-input [inpup]
+  (->> (str/split input  #"\n")
+       (mapv (fn [line] (vec (flatten (map #(map parse-long
+                                                 (str/split % #","))
+                                           (str/split line #"~"))))))))
+
+
+
+
 
 ;; too high 900
 ;; too high 895
