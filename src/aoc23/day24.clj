80fb1fea47d7a2c919d47eaf2236fd82e7dbb9fa
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2024-01-04 10:16:41 -0300

    day 24 part1

--- src/aoc23/day24.clj
+++ src/aoc23/day24.clj
@@ -1,4 +1,5 @@
 (ns aoc23.day24
+  "Never Tell Me The Odds"
   (:require [core :as c]
             [clojure.string :as s]))
 
@@ -21,21 +22,10 @@
 (defn next-point [[x y _ vx vy _]]
   [(+ x vx) (+ y vy)])
 
-(defn compute-line [pt1 pt2]
-  (let [[x1 y1] pt1
-        [x2 y2] pt2
-        m (/ (- y2 y1) (- x2 x1))]
+(defn compute-line [[x1 y1] [x2 y2]]
+  (let [m (/ (- y2 y1) (- x2 x1))]
     {:slope  m
      :offset (- y1 (* m x1))}))
-
-(def f (parse exp1-input))
-
-((:after? (first f)) [14.333 15.333])
-((:after? (second f)) [14.333 15.333])
-
-(compare -1 0)
-
-(and  (> 18 14.333) (> 19 15.333))
 
 (defn parse [inp]
   (->> (re-seq #"(?m)(.*)\,(.*)\,(.*)\@(.*)\,(.*)\,(.*)" inp)
@@ -48,8 +38,7 @@
                :pt1 [x y]
                :pt2 (next-point [x y z vx vy vz])
                :after? (after? [x y z vx vy vz])}))
-       (map (fn [{:keys [pt1 pt2] :as m}]
-              (merge m (compute-line pt1 pt2))))))
+       (map (fn [{:keys [pt1 pt2] :as m}] (merge m (compute-line pt1 pt2))))))
 
 
 (defn intercept [line1 line2]
@@ -61,22 +50,25 @@
                  (:offset line1))]
         [x y]))))
 
+(defn combinations [coll]
+  (->> (for [h1 coll h2 coll :when (not= h1 h2)]
+         #{h1 h2})
+       (into #{})
+       (map vec)))
+
 (defn part1 [inp rng]
-  (let [exp1 (parse inp)]
-    (->> (for [h1 exp1 h2 exp1 :when (not= h1 h2)]
-           #{h1 h2})
-         (into #{})
-         (map vec)
-         (keep (fn [[line1 line2]]
-                 (if-let [intersect (intercept line1 line2)]
-                   [(assoc line1 :intersect intersect)
-                    (assoc line2 :intersect intersect)])))
-         (filter (fn [[line1 line2]] (and ((:after? line1) (:intersect line1))
-                                          ((:after? line2) (:intersect line2)))))
-         (map first)
-         (map :intersect)
-         (filter rng)
-         (count))))
+  (->> (parse inp)
+       (combinations)
+       (keep (fn [[line1 line2]]
+               (when-let [intersect (intercept line1 line2)]
+                 [(assoc line1 :intersect intersect)
+                  (assoc line2 :intersect intersect)])))
+       (filter (fn [[line1 line2]] (and ((:after? line1) (:intersect line1))
+                                        ((:after? line2) (:intersect line2)))))
+       (map first)
+       (map :intersect)
+       (filter rng)
+       (count)))
 
 (comment
   (assert (= 2 (part1 exp1-input exp1-rng)))
