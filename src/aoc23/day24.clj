f97190a9a155b1c14ecb03d1fdf9b21a90a5ab4e
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2024-01-07 16:55:37 -0300

    Day 24 docs

--- src/aoc23/day24.clj
+++ src/aoc23/day24.clj
@@ -12,16 +12,24 @@
 (def part1-rng (partial in-range 200000000000000 400000000000000))
 (def exp1-rng (partial in-range 7 27))
 
-(defn after? [[x y _z vx vy _vz]]
-  (let [aftr #({-1 < 1 >} (compare % 0))]
-    (fn [[nx ny]]
-      (and ((aftr vx) nx x)
-           ((aftr vy) ny y)))))
+(defn after?
+  "Returns a function that given x-y coord, will return true if that coord 
+   happens after the line stating point"
+  [pt-or-number]
+  (if (number? pt-or-number)
+    ({-1 < 1 >} (compare pt-or-number 0))
+    (let [[x y _z vx vy _vz] pt-or-number]
+      (fn [[nx ny]]
+        (and ((after? vx) nx x) ((after? vy) ny y))))))
 
-(defn next-point [[x y _ vx vy _]]
+(defn next-point
+  "Given a starting point and velocity returns the next point"
+  [[x y _ vx vy _]]
   [(+ x vx) (+ y vy)])
 
-(defn compute-line [[x1 y1] [x2 y2]]
+(defn compute-line 
+  " If lines are not parallel return the slope and the offset of the line, this is the line equation"
+  [[x1 y1] [x2 y2]]
   (when-not (zero? (- x2 x1))
     (let [m (/ (- y2 y1)
                (- x2 x1))]
@@ -38,15 +46,13 @@
 (defn parse [inp]
   (->> (parse-raw inp)
        (map (fn [[x y z vx vy vz]]
-              {:raw [x y z vx vy vz]
-               :pt1 [x y]
+              {:pt1 [x y]
                :pt2 (next-point [x y z vx vy vz])
                :after? (after? [x y z vx vy vz])}))
        (map (fn [{:keys [pt1 pt2] :as m}]
               (merge m (compute-line pt1 pt2))))))
 
-
-(defn intercept [line1 line2]
+(defn intersect [line1 line2]
   (let [div (- (:slope  line2) (:slope  line1))]
     (if (zero? div)
       nil
@@ -56,15 +62,15 @@
         [x y]))))
 
 (defn combinations [coll]
-  (->> (for [h1 coll h2 coll :when (not= h1 h2)]
+  (->> (for [h1 coll
+             h2 coll
+             :when (not= h1 h2)]
          #{h1 h2})
        (into #{})
        (map vec)))
 
 (defn update-velocity [[x y vx vy] vel1 vel2]
   [x y (+ vx vel1) (+ vy vel2)])
-
-(defn to-v3  [[x y z vx vy vz]] {:x x :y y :z z :vx vx :vy vy :vz vz})
 
 (defn next-point-xy [[x y vx vy]]
   [(+ x vx) (+ y vy)])
@@ -76,54 +82,67 @@
 
 (defn all-hailstones-colide-at [hailstones vx vy]
   (loop [hailstones hailstones
-         colide-at nil]
-    (if (or (c/one? (count hailstones))
-            (empty? hailstones))
+         colide-at nil
+         ;; this is just so we can speed up, 
+         ;; so if 5 converge in the same point
+         ;; we stop and call it the day
+         speed-up 0]
+    (if (or (>= 1 (count hailstones))
+            (= speed-up 5))
       colide-at
       (let [h1-line (to-line (first hailstones) vx vy)
             h2-line (to-line (second hailstones) vx vy)
             hrest (rest (rest hailstones))]
         (when (and h1-line h2-line)
-          (when-let [intercept (intercept h1-line h2-line)]
-            (let [intercept-at (conj intercept vx vy)]
-              (cond (nil? colide-at) (recur hrest (conj intercept vx vy))
-                    (= intercept-at colide-at) (recur hrest intercept-at)))))))))
+          (when-let [intersect (intersect h1-line h2-line)]
+            (let [intersect-at (conj intersect vx vy)]
+              (cond (nil? colide-at) (recur hrest (conj intersect vx vy) (inc speed-up))
+                    (= intersect-at colide-at) (recur hrest intersect-at (inc speed-up))))))))))
 
-(defn relative-rock-finding [hailstones min-velocity max-velocity]
-  (let [velocities (range min-velocity (inc max-velocity))]
-    (->> (for [vx velocities
-               vy velocities
-               :when (or (not (zero? vx))
-                         (not (zero? vy)))]
-           (when-let [colide-at (all-hailstones-colide-at hailstones vx vy)]
-             colide-at))
-         (filter #(not (nil? %)))
-         (first))))
+(defn velocities []
+  (let [velocities (range -300 200)]
+    (for [vx velocities
+          vy velocities
+          :when (or (not (zero? vx))
+                    (not (zero? vy)))]
+      [vx vy])))
 
-(defn part1 [inp rng]
+(defn relative-rock-finding
+  "Changes hailstones velocity and check if all hailstones colide in the same point"
+  [hailstones]
+  (->> (velocities)
+       (keep (fn [[vx vy]] (all-hailstones-colide-at hailstones vx vy)))
+       (first)))
+
+(defn raw->x-y [[x y _z vx vy _vz]] [x y vx vy])
+(defn raw->x-z [[x _y z vx _vy vz]] [x z vx vz])
+
+(defn part1 
+  "Compare every line to each other, if line intersects and that 
+   intersection happen after the starting point of both lines and inside the predetermine zone we count that."
+  [inp rng]
   (->> (parse inp)
        (combinations)
        (keep (fn [[line1 line2]]
-               (when-let [intersect (intercept line1 line2)]
-                 [(assoc line1 :intersect intersect)
-                  (assoc line2 :intersect intersect)])))
-       (filter (fn [[line1 line2]] (and ((:after? line1) (:intersect line1))
-                                        ((:after? line2) (:intersect line2)))))
-       (map first)
-       (map :intersect)
+               (when-let [intersect (intersect line1 line2)]
+                 (when (and ((:after? line1) intersect) ((:after? line2) intersect))
+                   intersect))))
        (filter rng)
        (count)))
 
-(defn part2 [inp]
-  (let [raw (->> (parse-raw inp))
-        rawm (map to-v3 raw)
-        min-velocity (->> [:vx :vy :vz] (map #(c/min-by % rawm)) (reduce min))
-        max-velocity (->> [:vx :vy :vz] (map #(c/max-by % rawm)) (reduce max))
-        max-abs (max (abs min-velocity) (abs max-velocity))
-        raw-xy (map (fn [[x y _z vx vy _vz]] [x y vx vy]) raw)
-        raw-xz (map (fn [[x _y z vx _vy vz]] [x z vx vz]) raw)
-        [x y _ _] (relative-rock-finding raw-xy (- max-abs) max-abs)
-        [_ z _ _] (relative-rock-finding raw-xz (- max-abs) max-abs)]
+
+(defn part2
+  "Uses a technique that presumes that the rock is a static point, and changes the velocities of the hailstones
+   from the lower to upper bounds of available velocities. 
+   If all hailstones converge to a single point in a given updated velocity
+   we know that that point and the inverse of that velocity is what would be the starting position/velocity of 
+   the rock that would crash all the hailstones eventually"
+  [inp]
+  (let [raw (parse-raw inp)
+        raw-xy (map raw->x-y raw)
+        raw-xz (map raw->x-z raw)
+        [x y _ _] (relative-rock-finding raw-xy)
+        [_ z _ _] (relative-rock-finding raw-xz)]
     (+ x y z)))
 
 (comment
