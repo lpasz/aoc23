e565df6ca6d7984960fe9273dc8cc28869f046c2
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2024-01-08 22:36:31 -0300

    aoc 23 done

--- src/aoc23/day25.clj
+++ src/aoc23/day25.clj
@@ -15,66 +15,64 @@
        (map (fn [[key vals]] [key (vec vals)]))
        (into {})))
 
-(defn contraction
-  "Contract a edge between two nodes
-   A - C - D   >-contract->     AC - D
-   |           >-contract->     |
-   B           >-contract->     B
-   "
-  [m n1 n2]
-  (let [cluster-name (str n1 "-" n2)
-        e1 (get m n1)
-        e2 (get m n2)
-        cluster-edges (c/rejectv #{n1 n2} (into e1 e2))
-        m (-> m
-              (assoc cluster-name cluster-edges)
-              (dissoc n1)
-              (dissoc n2))]
-    (->> m
-         (map (fn [[key vals]]
-                [key (map #(get {n1 cluster-name n2 cluster-name} % %) vals)]))
-         (into {}))))
-
-;; A - B
-;; |   |
-;; C - D
-
 (def node1
+  ;; "A - B
+  ;;  |   |
+  ;;  C - D"
   {"a" ["b" "c"]
    "b" ["a" "d"]
    "c" ["a" "d"]
    "d" ["c" "b"]})
 
-;; A - B
-;; |   |
-;; C - D
-;; -----
-;; A - B
-;; | /
-;; CD
-
 (def node2
+  ;; "A - B
+  ;;  | \ |
+  ;;  C - D"
   {"a" ["b" "c" "d"]
    "b" ["a" "d"]
    "c" ["a" "d"]
    "d" ["c" "b" "a"]})
 
-;; A - B
-;; |   |
-;; C - D
-;; -----
-;; A - B
-;; | /
-;; CD
-(contraction node1 "d" "c")
+(defn contraction
+  "Contract a edge between two nodes
+   A - C - D   >-contract->     AC - D
+   |           >-contract->     |
+   B           >-contract->     B
 
-;; A - B
-;; | \ |
-;; C - D
-;; ------
-;; A - B
-;; || /
-;; CD
+   Other examples are like:
+   Example 1: 
+
+      A - B
+      |   |
+      C - D
+   --contract-- C and D
+      A - B
+      | /
+      CD
+
+   Example 2:
+    B - A
+    | / |
+    D - C
+    --contract-- C and D
+    A - B
+    || /
+    CD
+  "
+  [m n1 n2]
+  (let [cluster-name (str n1 "-" n2)
+        e1 (get m n1)
+        e2 (get m n2)
+        cluster-edges (c/rejectv #{n1 n2} (into e1 e2))]
+    (->> (-> m
+             (assoc cluster-name cluster-edges)
+             (dissoc n1)
+             (dissoc n2))
+         (map (fn [[key vals]]
+                [key (map #(get {n1 cluster-name n2 cluster-name} % %) vals)]))
+         (into {}))))
+
+(contraction node1 "d" "c")
 (contraction node2 "d" "c")
 
 (defn krager [g]
@@ -89,7 +87,6 @@
 
 (defn part1 [inp]
   (loop [g (parse inp)]
-    (c/insp ".")
     (if-let [g-reduced (krager g)]
       (->> g-reduced
            (keys)
@@ -100,6 +97,6 @@
 
 (comment
   (assert (= 54 (part1 exp1-input)))
-  (assert (= 543834 (part1 part1-input)))
+  (assert (= 543834 (time (part1 part1-input))))
   ;; there is no part 2 - it's done
   )
